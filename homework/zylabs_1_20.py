eb9d7c98975cf941e9403c9a45cc1f2dddf8474a
aahmad1212 <157183418+aahmad1212@users.noreply.github.com>
2024-02-07 23:58:19 -0600

    Add files via upload

---
+++ homework/zylabs_1_20.py
@@ -0,0 +1,9 @@
+# Adil Ahmad
+# 2278219
+user_num1 = int(input("Enter integer:"))
+print("\nYou entered:", user_num1)
+print(user_num1, "squared is", (user_num1 ** 2))
+print("And", user_num1, "cubed is", (user_num1 ** 3), "!!")
+user_num2 = int(input("Enter another integer:\n"))
+print(user_num1, "+", user_num2, "is", (user_num1 + user_num2))
+print(user_num1, "*", user_num2, "is", (user_num1 * user_num2))
