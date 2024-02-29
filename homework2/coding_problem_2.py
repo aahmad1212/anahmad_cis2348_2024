6efc9f829329ef1ca8a1a41031948b8bdb2eb173
aahmad1212 <157183418+aahmad1212@users.noreply.github.com>
2024-02-28 23:33:41 -0600

    Create coding_problem_2.py
    Part a

---
+++ homework2/coding_problem_2.py
@@ -0,0 +1,59 @@
+# Adil Ahmad
+# 2278219
+import datetime
+validmonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+               "November", "December"]
+validdays = ["1,", "2,", "3,", "4,", "5,", "6,", "7,", "8,", "9,", "10,", "11,", "12,", "13,", "14,", "15,", "16,",
+             "17,", "18,", "19,", "20,", "21,", "22,", "23,", "24,", "25,", "26,", "27,", "28,", "29,", "30,", "31,"]
+validyears = str(range(0, 10000))
+months = {
+    'January': 1,
+    'February': 2,
+    'March': 3,
+    'April': 4,
+    'May': 5,
+    'June': 6,
+    'July': 7,
+    'August': 8,
+    'September': 9,
+    'October': 10,
+    'November': 11,
+    'December': 12
+}
+today = datetime.date.today()
+datetoday = today.strftime("%B %d, %Y")
+datetoday_split = datetoday.split()
+input_date = None
+while input_date != "-1":
+    input_date = str(input())
+    input_split = input_date.split()
+    if input_date == "-1":
+        continue
+    if len(input_split) < 3:
+        continue
+    if input_split[0] not in validmonths and input_split[1] not in validdays and input_split[2] not in validyears:
+        continue
+    else:
+        input_split[1] = input_split[1].replace(",", "")
+        datetoday_split[1] = datetoday_split[1].replace(",", "")
+        if int(input_split[2]) <= int(datetoday_split[2]):
+            if int(input_split[2]) == int(datetoday_split[2]):
+                if months[input_split[0]] == months[datetoday_split[0]]:
+                    if int(input_split[1]) <= int(input_split[1]):
+                        output = str(months[input_split[0]]) + "/" + input_split[1] + "/" + input_split[2]
+                        print(output)
+                        continue
+                    else:
+                        continue
+                elif months[input_split[0]] > months[datetoday_split[0]]:
+                    continue
+                else:
+                    output = str(months[input_split[0]]) + "/" + input_split[1] + "/" + input_split[2]
+                    print(output)
+                    continue
+            else:
+                output = str(months[input_split[0]]) + "/" + input_split[1] + "/" + input_split[2]
+                print(output)
+                continue
+        else:
+            continue
